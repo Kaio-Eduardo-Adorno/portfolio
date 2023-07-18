d2b173dc576d7a4e3335b7c2a8d09bf0937c7266
Kaio-Eduardo-Adorno <kaioeduardoadorno@gmail.com>
2023-07-17 21:36:02 -0300

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,9 @@ import "./globals.css";
 import type { Metadata } from "next";
 import Link from "next/link";
 import { CustomLink } from "@/components/CustomLink";
-import localFont from 'next/font/local'
+import localFont from "next/font/local";
 
-const freedomFont = localFont({ src: '../../public/assets/fonts/Freedom.otf' })
+const freedomFont = localFont({ src: "../../public/assets/fonts/Freedom.otf" });
 
 export const metadata: Metadata = {
   title: "The Life Shit",
@@ -39,6 +39,15 @@ export default function RootLayout({
         <main className='p-4 flex-grow bg-main-400 bg-opacity-25'>
           {children}
         </main>
+        <footer className='px-4 py-2 flex justify-end items-center text-xs h-13 gap-2 shadow-sm'>
+          <Image
+            src='/assets/svgs/poop.svg'
+            width={20}
+            height={20}
+            alt='Poop'
+          />
+          <p className="-mb-1">© {new Date().getFullYear()} The Life Shit</p>
+        </footer>
       </body>
     </html>
   );
