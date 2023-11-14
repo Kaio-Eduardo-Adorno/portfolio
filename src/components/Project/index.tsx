c09e664ebf3b92ffeeb7a9fce987ca5f89d89b79
Kaio-Eduardo-Adorno <kaioeduardoadorno@gmail.com>
2023-11-14 04:47:52 -0300

    add projects to projects section

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -1,17 +1,22 @@
 import { IProject } from "@/constants/projects";
 import Image from "next/image";
+import Link from "next/link";
 
-export const Project: React.FC<IProject> = ({ title, image, tags }) => {
+export const Project: React.FC<IProject> = ({ title, image, url }) => {
   return (
-    <section className='p-1 basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5 2xl:basis-1/6'>
-      <div className='relative h-28 border-main-400 border-solid border-4 border-b-0 rounded-t-md'>
+    <a
+      href={url}
+      target='_blank'
+      className='p-1 basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5 2xl:basis-1/6'
+    >
+      <div className='relative h-36 border-main-400 border-solid border-4 border-b-0 rounded-t-md'>
         {image ? (
           <Image
             src={image}
             alt={title}
             fill
             unoptimized
-            className='object-cover'
+            className='object-contain'
           />
         ) : (
           <Image
@@ -25,8 +30,7 @@ export const Project: React.FC<IProject> = ({ title, image, tags }) => {
       </div>
       <div className='bg-main-400 p-1 flex flex-col rounded-b-md'>
         <span className='text-sm uppercase leading-freedom'>{title}</span>
-        {/* <span className='text-xs font-sans'>- {tags}</span> */}
       </div>
-    </section>
+    </a>
   );
 };
